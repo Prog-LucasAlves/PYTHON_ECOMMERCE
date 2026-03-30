fcb0c004c4e603d492d91f59cbebe0dc3274cbd7
Lucas <lucasalves_taz@hotmail.com>
2026-03-30 15:45:14 -0300

    up

---
+++ js/config.example.js
@@ -0,0 +1,13 @@
+﻿// Firebase Configuration Template
+// Copy this file to config.js and fill in your actual Firebase credentials
+// config.js is ignored by git and should never be committed
+
+export const firebaseConfig = {
+  apiKey: "INSERT_YOUR_API_KEY_HERE", // pragma: allowlist secret
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.firebasestorage.app",
+  messagingSenderId: "0000000000",
+  appId: "1:0000000000:web:0000000000000000",
+  measurementId: "G-00000000"
+};
